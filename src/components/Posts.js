bd750fd496b18273440f74f1587954bfcce72aae
Rafael Prazeres <rafael.prazeres@ufrn.br>
2023-05-26 00:32:02 -0300

    The React Mega-Tutorial, Chapter 5: Connecting to a Back End (2)

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,29 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Spinner from "react-bootstrap/Spinner";
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function Posts() {
     const [posts, setPosts] = useState();
 
-    //TODO: add a side effect function to request posts here
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(BASE_API_URL + '/api/feed')
+            if (response.ok) {
+                const results = await response.json();
+                setPosts(results.data);
+            }
+            else {
+                setPosts(null);
+            }
+        })();
+    }, []);
 
     return (
         <>
             {posts === undefined ?
                 <Spinner animation="border" />
-            :
+                :
                 <>
-                    {posts.length !== 0 ?
-                        posts.map(post => {
-                            return (
-                                <p key={post.id}>
-                                    <b>{post.author.username}</b> &mdash; {post.timestamp}
-                                    <br />
-                                    {post.text}
-                                </p>
-                            );
-                        })
+                    {posts === null ?
+                        <p>Could not retrieve blog posts.</p>
                         :
-                        <p>There are no blog posts.</p>
+                        <>
+                            {posts.length !== 0 ?
+                                posts.map(post => {
+                                    return (
+                                        <p key={post.id}>
+                                            <b>{post.author.username}</b> &mdash; {post.timestamp}
+                                            <br />
+                                            {post.text}
+                                        </p>
+                                    );
+                                })
+                                :
+                                <p>There are no blog posts.</p>
+                            }
+                        </>
                     }
                 </>
             }
