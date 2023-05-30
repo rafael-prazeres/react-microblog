80ea00e7d14980471d7b16b9d466c54497d58c97
Rafael Prazeres <rafaelprazeres@ect.ufrn.br>
2023-05-30 16:11:26 -0300

    The React Mega-Tutorial, Chapter 7: Forms and Validation (1)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,37 @@
+import { useState, useEffect, useRef } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import Body from '../components/Body';
+import InputField from '../components/InputField';
 
 export default function LoginPage() {
+    const [formErrors, setFormErrors] = useState({});
+    const usernameField = useRef();
+    const passwordField = useRef();
+
+    useEffect(() => {
+        usernameField.current.focus();
+    }, []);
+
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        const username = usernameField.current.value;
+        const password = passwordField.current.value;
+        console.log(`You entered ${username}:${password}`);
+    };
+
     return (
-        <Body sidebar>
-            <h1>Login form</h1>
-            <p>TODO</p>
+        <Body>
+            <h1>Login</h1>
+            <Form onSubmit={onSubmit}>
+                <InputField
+                    name="username" label="Username or email address"
+                    error={formErrors.username} fieldRef={usernameField} />
+                <InputField
+                    name="password" label="Password" type="password"
+                    error={formErrors.password} fieldRef={passwordField} />
+                <Button variant="primary" type="submit">Login</Button>
+            </Form>
         </Body>
     );
 }
