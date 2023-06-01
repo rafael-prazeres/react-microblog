8df7c6a4dd2baaf50e7bd70019876b013a6ed5f6
Rafael Prazeres <rafaelprazeres@ect.ufrn.br>
2023-06-01 15:10:24 -0300

    The React Mega-Tutorial, Chapter 8: Authentication (1)

--- src/MicroblogApiClient.js
+++ src/MicroblogApiClient.js
@@ -17,6 +17,7 @@ export default class MicroblogApiClient {
                 method: options.method,
                 headers: {
                     'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + localStorage.getItem('accessToken'),
                     ...options.headers,
                 },
                 body: options.body ? JSON.stringify(options.body) : null,
@@ -55,6 +56,28 @@ export default class MicroblogApiClient {
 
     async delete(url, options) {
         return this.request({method:'DELETE', url, ...options});
+    }
+
+    async login(username, password) {
+        const response = await this.post('/tokens', null, {
+            headers: {
+                Authorization: 'Basic ' + btoa(username + ":" + password)
+            }
+        });
+        if (!response.ok) {
+            return response.status === 401 ? 'fail' : 'error';
+        }
+        localStorage.setItem('accessToken', response.body.access_token);
+        return 'ok';
+    }
+
+    async logout() {
+        await this.delete('/tokens');
+        localStorage.removeItem('accessToken');
+    }
+
+    isAuthenticated() {
+        return localStorage.getItem('accessToken') !== null;
     }
 
 }
