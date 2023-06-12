0d92654bbba74e34c3fbb9d51f6d9fba85c1e8e1
Rafael Prazeres <rafael.prazeres@ufrn.br>
2023-06-11 22:17:10 -0300

    The React Mega-Tutorial, Chapter 11: Automated Testing (1)

---
+++ src/components/Post.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Post from './Post';
+
+test ('it renders all the components of the post', () => {
+    const timestampUTC = '2023-06-11T22:00:00.000Z';
+    const post = {
+        text: 'hello',
+        author: {username: 'susan', avatar_url: 'https://example.com/avatar/susan'},
+        timestamp: timestampUTC,
+    };
+
+    render (
+        <BrowserRouter>
+            <Post post={post} />
+        </BrowserRouter>
+    );
+
+    const message = screen.getByText('hello');
+    const authorlink = screen.getByText('susan');
+    const avatar = screen.getByAltText('susan');
+    const timestamp = screen.getByText(/.* ago$/);
+
+    expect(message).toBeInTheDocument();
+    expect(authorlink).toBeInTheDocument();
+    expect(authorlink).toHaveAttribute('href', '/user/susan');
+    expect(avatar).toBeInTheDocument();
+    expect(avatar).toHaveAttribute('src', 'https://example.com/avatar/susan&s=48');
+    expect(timestamp).toBeInTheDocument();
+    expect(timestamp).toHaveAttribute(
+        'title', new Date(Date.parse(timestampUTC)).toString());
+});
